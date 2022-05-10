33b3a2f30801b442b7317a241133c40c86b0e92a
Rajbhau00 <105200511+Rajbhau00@users.noreply.github.com>
2022-05-10 16:42:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,12 +17,12 @@ APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
 async def start(client: pr0fess0r_99, message: Message):
     approvedbot = await client.get_me() 
     button=[[
-      InlineKeyboardButton("📦 Repo", url="https://github.com/PR0FESS0R-99/Auto-Approved-Bot"),
+      InlineKeyboardButton("📦 Repo", url="https://github.com/Rajbhau00/Auto-Approved-Bot"),
       InlineKeyboardButton("Updates 📢", url="t.me/Mo_Tech_YT")
       ],[
       InlineKeyboardButton("➕️ Add Me To Your Chat ➕️", url=f"http://t.me/{approvedbot.username}?startgroup=botstart")
       ]]
-    await message.reply_text(text="**__Hello Iam Auto Approved Join Request Bot Repo https://github.com/PR0FESS0R-99/Auto-Approved-Bot**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
+    await message.reply_text(text="**__Hello Iam Auto Approved Join Request Bot Repo https://github.com/Rajbhau00/Auto-Approved-Bot**__", reply_markup=InlineKeyboardMarkup(button), disable_web_page_preview=True)
 
 @pr0fess0r_99.on_chat_join_request(filters.chat(CHAT_ID))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
